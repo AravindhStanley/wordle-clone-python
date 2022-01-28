ba56a9dc32c7ee5e739c51291e0fca953fbb46b6
AravindhStanley <27763368+AravindhStanley@users.noreply.github.com>
2022-01-28 12:32:52 +0530

    Initial Working Release
    A working wordle clone. Uses Rich to print highlighted tables.

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from rich.console import Console
+from rich.table import Table
+from rich.live import Live
+from src.modules.wordle import Wordle
+
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+if __name__ == "__main__":
+    puzzle = Wordle()
+    console = Console()
+    table = Table(title="Guesses", box=None)
+    console.print(table)
+
+retries_pending = 6
+
+while retries_pending:
+    puzzle.get_user_guess(remaining=retries_pending)
+    status, result = puzzle.check_word()
+    clear()
+    with Live(table):
+        msg_row = [f'[black on {i["color"]}] {i["letter"]} [/black on {i["color"]}]' for i in result]
+        table.add_row(*msg_row)
+        table.add_row("")
+    if status:
+        retries_pending = 0
+        print(console.print('\n :thumbs_up: Wow, you aced it!! \n'))
+    else:
+        retries_pending -= 1
+
+if not status:
+    console.print(f'\n\n☹️  [bold red]Correct Word is {puzzle.chosen_word.upper()} [/bold red]')
---
+++ src/modules/wordle.py
@@ -0,0 +1,59 @@
+import random
+from rich.prompt import Prompt
+from rich import print
+from .words import words
+
+
+class Wordle:
+
+    def __init__(self):
+        self.words:list = words.get('five_letter')
+        self.chosen_word:str = self._select_word()
+        self.max_words = 5
+        self.user_guess:str = ""
+
+
+    def get_user_guess(self, remaining:int = None):
+        self.user_guess = Prompt.ask(f"\n\n[gray]Guess your word ({remaining} guess left) [/gray]").strip()
+        if len(self.user_guess) != self.max_words:
+            print('\n [red]--- Wait a minute.... That ain\'t a five letter word !!!! --- \n')
+            self.get_user_guess()
+        elif self.user_guess not in self.words:
+            print('\n [red]--- Oops! Not a valid word!!!! --- \n')  
+            self.get_user_guess()
+        
+    def _select_word(self):
+        random_index = int(
+            random.random() * len(self.words)
+            )
+        return self.words[random_index].lower()
+
+    def is_correct_guess(self):
+        if self.chosen_word.lower() == self.user_guess.lower():
+            return True
+        return False
+
+    def check_word(self):
+        user_guess_validated  = []
+        user_guess = list(self.user_guess)
+        system_word = list(self.chosen_word)
+        # print(Columns(user_guess))
+            
+
+        for idx, user_word in enumerate(user_guess):
+            temp = {'letter': user_word}
+            if user_word == system_word[idx]:
+                temp['color'] = 'spring_green3'
+            elif user_word in system_word:
+                temp['color'] = 'orange1'
+            else:
+                temp['color'] = 'grey84'
+            user_guess_validated.append(temp)
+
+        ## Check if the word is correct directly
+        if self.is_correct_guess():
+            return True, user_guess_validated
+
+        return False, user_guess_validated
+
+
