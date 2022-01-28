1603fde8112029c36f89acb6b804e96c5d7caac1
AravindhStanley <27763368+AravindhStanley@users.noreply.github.com>
2022-01-28 12:41:15 +0530

    Fixed Indentation
    Move all the code block inside __main__ check.

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ if __name__ == "__main__":
     table = Table(title="Guesses", box=None)
     console.print(table)
 
-retries_pending = 6
+    retries_pending = 6
 
-while retries_pending:
-    puzzle.get_user_guess(remaining=retries_pending)
-    status, result = puzzle.check_word()
-    clear()
-    with Live(table):
-        msg_row = [f'[black on {i["color"]}] {i["letter"]} [/black on {i["color"]}]' for i in result]
-        table.add_row(*msg_row)
-        table.add_row("")
-    if status:
-        retries_pending = 0
-        print(console.print('\n :thumbs_up: Wow, you aced it!! \n'))
-    else:
-        retries_pending -= 1
+    while retries_pending:
+        puzzle.get_user_guess(remaining=retries_pending)
+        status, result = puzzle.check_word()
+        clear()
+        with Live(table):
+            msg_row = [f'[black on {i["color"]}] {i["letter"]} [/black on {i["color"]}]' for i in result]
+            table.add_row(*msg_row)
+            table.add_row("")
+        if status:
+            retries_pending = 0
+            print(console.print('\n :thumbs_up: Wow, you aced it!! \n'))
+        else:
+            retries_pending -= 1
 
-if not status:
-    console.print(f'\n\n☹️  [bold red]Correct Word is {puzzle.chosen_word.upper()} [/bold red]')
+    if not status:
+        console.print(f'\n\n☹️  [bold red]Correct Word is {puzzle.chosen_word.upper()} [/bold red]')
